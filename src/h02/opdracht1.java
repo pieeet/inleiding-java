de39ef93d5647c1e3caa5fc59cb1bd53546d2d5b
Mark Bijl <m>
2019-10-01 14:12:50 +0200

    H2O1 klaar

---
+++ src/h02/opdracht1.java
@@ -0,0 +1,20 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+
+
+public class opdracht1 extends Applet {
+
+    public void init(){
+        setBackground(Color.BLUE);
+
+    }
+    public void paint(Graphics g){
+
+        g.setColor(Color.YELLOW);
+        g.drawString("Mark", 50,60);
+
+    }
+}
