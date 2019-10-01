4f9f9de1185d40ec84d1faa384472647167cde2f
Mark Bijl <m>
2019-10-01 14:30:12 +0200

    H2 vragen gemaakt. Staan als commentaar in opdracht2.

--- src/h02/opdracht2.java
+++ src/h02/opdracht2.java
@@ -2,7 +2,14 @@ package h02;
 
 import java.awt.*;
 import java.applet.*;
-
+/* Vragen Hoofdstuk 2
+    1: Machinetaal is computertaal; nullen en enen.
+    2: Een editor om de code mee te schrijven, een compiler om de code te vertalen naar machine taal en
+    als laatst heb je een linker nodig.
+    3: Vrijwel alles wat in het blauw is dikgedrukt is een opdracht.
+    4: Het importeren van beschikbare software uit je bibliotheek.
+    5: Een methode is een verzameling opdrachten onder een naam.
+ */
 
 
 public class opdracht2 extends Applet {
