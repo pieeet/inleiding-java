8e6cd03410575dab7f0db2f345e06dcf0747fd1c
Mark Bijl <m>
2019-10-03 10:37:43 +0200

    Opdracht 4.06 af

--- src/h04/Opdracht406.java
+++ src/h04/Opdracht406.java
@@ -10,7 +10,17 @@ public class Opdracht406 extends Applet {
     }
 
     public void paint (Graphics g) {
-        g.fillRoundRect(20,20,50,100,10,10);
+
+        g.fillRoundRect(150,50,100,240,25,20);
+
+        g.setColor(Color.GREEN);
+        g.fillArc(170,65,60,60,0,360);
+
+        g.setColor(Color.ORANGE);
+        g.fillArc(170,135,60,60,0,360);
+
+        g.setColor(Color.RED);
+        g.fillArc(170,205,60,60,0,360);
     }
 
 
