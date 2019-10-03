21177bf89cc57b7bc49c27f52fbc965154aa460c
Mark Bijl <m>
2019-10-03 10:21:16 +0200

    Opdracht 4.05 af

---
+++ src/h04/Opdracht405.java
@@ -0,0 +1,34 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht405 extends Applet {
+
+    public void init(){
+   setBackground(Color.BLUE);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.YELLOW);
+        g.fillArc(100,50,80,40,0,360);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
