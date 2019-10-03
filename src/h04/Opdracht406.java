4bfc7124c7ab0371acb3a623589083f6a787a964
Mark Bijl <m>
2019-10-03 10:27:35 +0200

    Opdracht 4.05 bug fix

---
+++ src/h04/Opdracht406.java
@@ -0,0 +1,17 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht406 extends Applet {
+
+    public void init (){
+
+    }
+
+    public void paint (Graphics g) {
+        g.fillRoundRect(20,20,50,100,10,10);
+    }
+
+
+}
