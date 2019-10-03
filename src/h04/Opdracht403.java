cddaf82f1096bd84c7d59bc01fa55a3e4a4d82e4
Mark Bijl <m>
2019-10-03 09:12:59 +0200

    Opdracht 4.03 af

--- src/h04/Opdracht403.java
+++ src/h04/Opdracht403.java
@@ -1,4 +1,24 @@
 package h04;
 
-public class Opdracht403 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht403 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.CYAN);
+        g.drawLine(200,100,200,300);
+        g.setColor(Color.RED);
+        g.fillRect(100,100,100,25);
+        g.setColor(Color.WHITE);
+        g.fillRect(100,125,100,25);
+        g.setColor(Color.BLUE);
+        g.fillRect(100,150,100,25);
+
+
+    }
 }
