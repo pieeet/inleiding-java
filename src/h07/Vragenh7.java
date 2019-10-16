6ddf7370637908aa4542722405134dba395cc1cb
Mark Bijl <m>
2019-10-16 09:07:09 +0200

    Vragen H7 gemaakt.

---
+++ src/h07/Vragenh7.java
@@ -0,0 +1,12 @@
+package h07;
+
+/*
+1. Dankzij de uitwerkingen heb ik het uiteindelijk weten te vinden. Hierdoor kan ik het in het vervolg zelf ook vinden.
+2. Hij wordt hier gedefinieerd: Methods inherited from class >>>java.awt.Component<<<
+3. Het verschil is de manier waarop ze worden aangeroepen, maar ze doen volgens mij hetzelfde.
+ */
+
+
+
+public class Vragenh7 {
+}
