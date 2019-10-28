892b866251241125d24814ccba4d5fbdca82381d
Mark Bijl <m>
2019-10-28 11:42:27 +0100

    Opdracht1101 klaar

--- src/h11/Opdracht1101.java
+++ src/h11/Opdracht1101.java
@@ -1,5 +1,13 @@
 package h11;
 
+/*Vragen hoofdstuk 11
+1. Een For is de kortere versie van de While
+2. Lussen zijn eigenlijk statements in statements in statements.
+3. Nee, hij eigenlijk zegt "wanneer 10 kleiner is als 5 dan..." en dat is het niet.
+
+ */
+
+
 import java.applet.Applet;
 import java.awt.*;
 
