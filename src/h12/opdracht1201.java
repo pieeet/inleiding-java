b923d7c11ddaef1d78f9a09dd177c0bdf08beb09
Mark Bijl <m>
2019-12-17 13:38:04 +0100

    8.??  niet klaar 11.12 niet klaar
    Verder tot 12.01 klaar.

--- src/h12/opdracht1201.java
+++ src/h12/opdracht1201.java
@@ -13,9 +13,9 @@ int gemiddelde;
 
     public void init() {
 
-        cijfers = new int[5];
+        cijfers = new int[10];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             cijfers[i] = (int) (Math.random() * 10 + 1);
             System.out.println(cijfers[i] + "-");
         }
@@ -32,7 +32,8 @@ int gemiddelde;
         for (int i = 0; i < cijfers.length; i++) {
          g.drawString(" " + cijfers[i], x, y);
          x += 15;
-         gemiddelde = (cijfers[0] + cijfers [1] + cijfers [2] + cijfers [3] + cijfers [4]) / 5;
+         gemiddelde = (cijfers[0] + cijfers [1] + cijfers [2] + cijfers [3] + cijfers [4] + cijfers [5] + cijfers [6] +
+                 cijfers [7] + cijfers [8] + cijfers [9]) / 10;
 //         g.drawString(" Het gemiddelde is: " + gemiddelde,50,65);
         }
 
